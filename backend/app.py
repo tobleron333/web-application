b7c34bdca0a610230ed7005f85a5cf8ad872cc5d
poctuclabka <rostislavkasan@mail.ru>
2025-11-08 22:11:55 +0300

    500 чанк.py

--- backend/app.py
+++ backend/app.py
@@ -79,7 +79,7 @@ def process_csv():
 
     file = request.files['file']
 
-    chunk_size = 50
+    chunk_size = 500
     results = []
 
     for chunk in pd.read_csv(file, sep=';', chunksize=chunk_size, dtype={'№ вопроса': 'Int64'}):
