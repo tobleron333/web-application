2ac8391e302111f8b1c829a073688e8684d39b00
tobleron333 <aharcev706@gmail.com>
2025-11-09 20:23:54 +0300

    Чанк 1000 app.py

--- backend/app.py
+++ backend/app.py
@@ -76,7 +76,7 @@ def process_csv():
         return {'error': 'Файл не загружен'}, 400
 
     file = request.files['file']
-    chunk_size = 300
+    chunk_size = 1000
     results = []
 
     for chunk in pd.read_csv(file, sep=';', chunksize=chunk_size, dtype={'№ вопроса': 'Int64'}):
